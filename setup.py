4cdcb68dc761220d568cd2701d48871672465e60
Oliver Tengler <otengler@gmx.de>
2022-06-01 10:14:02 +0200

    Add executable bit, remove excludes for Mac

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ import os
 from cx_Freeze import setup, Executable
 import AppConfig
 
+options = {
+    "build_exe": {
+        "zip_include_packages": "logging,http,collections,encodings,sqlite3,unittest,urllib,asyncio,concurrent,dialogs,email,importlib"
+    }
+}
+
 targetBase = None
 if os.name == "nt":
     targetBase = "Win32GUI"
     targetCodeBeagle = "CodeBeagle.exe"
     targetUpdateIndex = "UpdateIndex.exe"
+    options["build_exe"]["excludes"] = "distutils,html,multiprocessing,lib2to3,xml,test,tkinter,pydoc_data,bz2,queue,lzma,ssl"
 else:
     targetBase = None
     targetCodeBeagle = "CodeBeagle"
@@ -23,13 +30,6 @@ UpdateIndex=Executable(
     script = "UpdateIndex.py",
     targetName = targetUpdateIndex
 )
-
-options = {
-    "build_exe": {
-        "zip_include_packages": "logging,http,collections,encodings,sqlite3,unittest,urllib,asyncio,concurrent,dialogs,email,importlib",
-        "excludes": "distutils,html,multiprocessing,lib2to3,xml,test,tkinter,pydoc_data,bz2,queue,lzma,ssl"
-    }
-}
 
 setup(
     options=options,
