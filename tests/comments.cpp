0a958714743e97964c207e67caaaab17f4ea16b1
Oliver Tengler <otengler@gmx.de>
2025-04-26 20:12:38 +0200

    Comment test file

---
+++ tests/comments.cpp
@@ -0,0 +1,31 @@
+int i = 0;
+
+// single line comment
+
+i = 1;
+
+/*
+multiline comment
+*/
+
+i = 2;
+
+/*
+// comment inside multi
+*/
+
+// /* test  */
+
+i = 5;
+
+// a
+
+i = 10;
+
+// /* a
+
+i = 20;
+
+// */ 
+
+i = 30;
