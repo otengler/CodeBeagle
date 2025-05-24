2da5fb5db900abcda76902b7ad78ccb042d95956
Oliver Tengler <otengler@gmx.de>
2025-05-24 14:10:20 +0200

    Simply code

--- tools/CustomFont.py
+++ tools/CustomFont.py
@@ -1,22 +1,12 @@
-from typing import Optional
 import os
-from PyQt5.QtGui import QFontDatabase, QFont
-
-customFont: Optional[QFont] = None
+from PyQt5.QtGui import QFontDatabase
 
 def installCustomFont():
     fontDir = os.path.join("resources", "font")
     try:
-        global customFont
-        customFont = None
         for name in os.listdir(fontDir):
             fullPath = os.path.join(fontDir, name)
             if os.path.isfile(fullPath) and fullPath.lower().endswith(".ttf"):
                 QFontDatabase.addApplicationFont(fullPath)
-                customFont = os.path.splitext(name[0])
-                break
     except:
         pass
-
-def customLFontList():
-    return customFont
