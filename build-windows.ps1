64a43021e1b5bb37810f9c94b27495a653a8036f
Oliver Tengler <otengler@gmx.de>
2025-12-06 12:08:09 +0100

    Automatically compress output for local build

--- build-windows.ps1
+++ build-windows.ps1
@@ -1,2 +1,16 @@
 & .\.venv\Scripts\Activate.ps1
-& .\build-windows-freeze.ps1  
+& .\build-windows-freeze.ps1
+
+#
+# Compress the build folder
+#
+$versionData = Get-Content VERSION | ConvertFrom-Json
+$version = $versionData.local
+$zipFileName = "build\CodeBeagle.$version.zip"
+
+if (Test-Path $zipFileName) {
+    Remove-Item $zipFileName -Force
+}
+
+Compress-Archive -Path "build\CodeBeagle" -DestinationPath $zipFileName
+echo "Created $zipFileName"  
